203785fc9650a208197199a669c6f124b2fa811b
user <info@786infinite.com>
2026-05-12 18:46:57 +0100

    Update robots rules

--- app/robots.ts
+++ app/robots.ts
@@ -2,10 +2,16 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: [
+          "/go/",
+          "/affiliate-tracker",
+        ],
+      },
+    ],
     sitemap: "https://solarcal.co.uk/sitemap.xml",
   };
 }
